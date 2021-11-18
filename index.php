fc616c9a0704c21fc1f052d4abd9ffe756f505dd
IntanNuraudhatul <81946507+IntanNuraudhatul@users.noreply.github.com>
2021-11-18 09:44:47 +0800

    crud database

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once("config.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<html>
+
+<head>
+	<title>CRUD</title>
+</head>
+
+<body>
+	<center>
+		<h1>Student Record</h1>
+	</center>
+
+		<a href="add.html">ADD NEW DATA</a><br /><br />
+
+		<center>
+			<table width='80%' border=0>
+
+				<tr bgcolor='#CCCCCC'>
+					<td>NAME</td>
+					<td>AGE</td>
+					<td>EMAIL</td>
+					<td>UPDATE</td>
+				</tr>
+				<?php
+
+				while ($res = mysqli_fetch_array($result)) {
+					echo "<tr>";
+					echo "<td>" . $res['nama'] . "</td>";
+					echo "<td>" . $res['umur'] . "</td>";
+					echo "<td>" . $res['email'] . "</td>";
+					echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('ARE YOU SURE WANT TO DELETE?')\">Delete</a></td>";
+				}
+				?>
+			</table>
+		</center>
+</body>
+
+</html>
